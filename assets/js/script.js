704ad4ea43995aa2a9b39061643b3c095e978197
JuanQuezada <ju.quezada.c@gmail.com>
2021-09-27 22:16:31 -0300

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 class Propietario {
     constructor(nombre, direccion, telefono){
-        this._nombre = nombre;
-        this._direccion = direccion;
-        this._telefono = telefono;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
     } 
     datosPropietario(){
         return `El Nombre del dueño es: ${this.nombre}. El Domicilio es: ${this.direccion}, y el numero telefonico de contacto: ${this.telefono}`
@@ -10,27 +10,27 @@ class Propietario {
 };
 
 class Animal extends Propietario {       //clase hijo de propietario
-    constructor (tipo){
+    constructor (nombre, direccion, telefono, tipo){
+        super(nombre, direccion, telefono)
         this._tipo = tipo; 
     }
     get tipo () {
-        return this._tipo;
+        return `El tipo de animal es un: ${this._tipo}`;
     }
-    // set tipo(tipo_nuevo) {
-    //     this._tipo = tipo_nuevo;
-    // }
+    
 }
 
-class Mascota extends Animal {
-    constructor (nombreMascota, enfermedad){
+class Mascota extends Animal { //clase hijo de animal, nieto de la clase propietario
+    constructor (nombre, direccion, telefono, tipo, nombreMascota, enfermedad){
+        super(nombre, direccion, telefono, tipo)
         this._nombreMascota = nombreMascota;
         this._enfermedad = enfermedad;
     }
     get nombreMascota(){
         return this._nombreMascota;
     }
-    set nombreMascota (nombreMascotaNuevo){
-        this._nombreMascota = nombreMascotaNuevo;
+    set nombreMascota (nombreMascotaNueva){
+        this._nombreMascota = nombreMascotaNueva;
     }
     get enfermedad(){
         return this._enfermedad;
@@ -39,26 +39,25 @@ class Mascota extends Animal {
         this._enfermedad = enfermedadNueva;
     }
 }
-let agregar = document.getElementsByTagName('button');
 
-agregar.addEventListener('click', observando);
+let agregar = document.querySelector('form')
 
-function agregando(){
-    let nombre = document.getElementById("propietario").value;
-    let direccion = document.getElementById("telefono").value;
-    let telefono = document.getElementById("direccion").value;
-    let tipo = document.getElementById("tipo").value
-    let nombreMascota = document.getElementById("nombreMascota").value;
-    let enfermedad = document.getElementById("direccion").value;
+let lista = (mascota) => {
+    return `<li>${mascota.datosPropietario()}</li><li>${mascota.tipo}, mientras que el nombre de la mascota es: ${mascota.nombreMascota} y la enfermedad es ${mascota.enfermedad}</li>`
 
 }
 
-function observando() {
-    const nombreData = agregando();
-    const nombre = nombreData.nombre;
-    const direccion = nombreData.direccion;
-    const telefono = nombreData.telefono;
-    const tipo = nombreData.tipo;
-    const nombreMascota = nombreData.nombreMascota;
-    const enfermedad = nombreData.enfermedad;
-}
+agregar.addEventListener('submit', (observando) => {
+    observando.preventDefault()
+    let nombre = document.querySelector("#propietario").value;
+    let direccion = document.querySelector("#direccion").value;
+    let telefono = document.querySelector("#telefono").value;
+    let tipo = document.querySelector("#tipo").value;
+    let nombreMascota = document.querySelector("#nombreMascota").value;
+    let enfermedad = document.querySelector("#enfermedad").value;
+    let resultado = document.querySelector('ul');
+
+    let mascota = new Mascota (nombre, direccion, telefono, tipo, nombreMascota, enfermedad)
+    resultado.innerHTML = lista(mascota)
+})
+
